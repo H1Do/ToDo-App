ebf8e72cc1e33e4d6c9045d91688b4fe036c8d56
Niyaz Latypov <hidouchiha@gmail.com>
2024-03-22 23:25:46 +0500

    feat: init project

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import './styles/styles.scss';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  
+
+  <div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="#">Your Name Here</a>.
+  </div>
+`;
